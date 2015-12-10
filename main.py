2629cc44ee0ffee26a3b80eb9eab3cf1ce564915
Rui Pires <r.pires@digisource.pt>
2015-12-10 18:25:14 +0000

    when the final file exists, ask the user to overwrite or select a new name

--- main.py
+++ main.py
@@ -1473,7 +1473,7 @@ class MainWindow(wx.Frame):
         self.button_sizer = wx.BoxSizer(wx.HORIZONTAL)
         self.open_playlist_btn = wx.Button(parent=self.panel, id=wx.ID_ANY, label=t("Open File"))
         self.quit_app_btn = wx.Button(parent=self.panel, id=wx.ID_ANY, label=t("Quit"))
-        self.button_sizer.Add(self.open_playlist_btn)
+        self.button_sizer.Add(self.open_playlist_btn, 0, wx.RIGHT, 10)
         self.button_sizer.Add(self.quit_app_btn)
         self.main_sizer.Add(self.button_sizer, 0, wx.ALIGN_CENTER | wx.ALL, 10)
 
@@ -1516,7 +1516,9 @@ class MainWindow(wx.Frame):
         self.start_time = ""
         self.end_time = ""
 
+        self.out_path = ""
         self.final_path = ""
+        self.file_crl = wx.FileCtrl()
 
         # version_thr = CheckForUpdate(version, self.temp_dir)
         # version_thr.start()
@@ -2001,16 +2003,40 @@ class MainWindow(wx.Frame):
 
         # outfile
         out_filename = self.base_name.replace(".vopl", "")
-        out_path = self.final_destination_path + path_separator + out_filename + ".mp4"
+        self.out_path = self.final_destination_path + path_separator + out_filename + ".mp4"
+        self.final_path = self.out_path
 
-        if os.path.isfile(out_path):
-            rnd = random.randint(1, 5)
-            out_path = self.final_destination_path + path_separator + out_filename + "_" + str(rnd) + "_" + ".mp4"
+        if os.path.isfile(self.out_path):
+            # ask for overwrite?
+            overwrite_dlg = wx.Dialog(parent=self, id=wx.ID_ANY, title=t("Overwrite final file?"))
+            overwrite_dlg_sizer = wx.BoxSizer(wx.VERTICAL)
+            overwrite_msg = wx.StaticText(parent=overwrite_dlg, id=wx.ID_ANY,
+                                     label=t("There is already a file with that name on the destination directory... Do you wish to:"))
+            overwrite_btn = wx.Button(parent=overwrite_dlg, id=wx.ID_ANY, label=t("Overwrite"))
+            rename_btn = wx.Button(parent=overwrite_dlg, id=wx.ID_OK, label=t("Rename"))
+            # sizer stuff
+            overwrite_dlg_sizer.Add(overwrite_msg, 0, wx.ALL, 5)
+
+            done_dlg_btn_sizer = wx.BoxSizer(wx.HORIZONTAL)
+            done_dlg_btn_sizer.Add(overwrite_btn, 0, wx.RIGHT, 10)
+            done_dlg_btn_sizer.Add(rename_btn)
+
+            overwrite_dlg_sizer.Add(done_dlg_btn_sizer, 0, wx.ALIGN_CENTER | wx.TOP | wx.BOTTOM, 10)
+            overwrite_dlg.SetSizer(overwrite_dlg_sizer)
+            # auto layout TODO fix this a bit
+            # done_dlg.SetAutoLayout(1)
+            # done_dlg.Fit()
+
+            overwrite_dlg_sizer.SetSizeHints(overwrite_dlg)
+
+            # bind
+            overwrite_dlg.Bind(event=wx.EVT_BUTTON, handler=self.overwrite_out_path, source=overwrite_btn)
+            overwrite_dlg.Bind(event=wx.EVT_BUTTON, handler=self.rename_out_path, source=rename_btn)
+            # and show
+            overwrite_dlg.ShowModal()
 
         # put it on desktop for now
-        join_args.append("" + out_path + "")
-
-        self.final_path = out_path
+        join_args.append("" + self.final_path + "")
 
         # sys.stdout.write("JOINARGS>>" + ' '.join(join_args))
 
@@ -2061,6 +2087,37 @@ class MainWindow(wx.Frame):
         done_dlg.Bind(event=wx.EVT_BUTTON, handler=self.open_file_with_app, source=done_open_btn)
         # and show
         done_dlg.Show()
+
+    def overwrite_out_path(self, e):
+        # close the choice modal and set final path to overwrite
+        self.final_path = self.out_path
+        e.EventObject.Parent.Destroy()
+
+    def rename_out_path(self, e):
+        # close the choice modal
+        e.EventObject.Parent.Destroy()
+
+        # dialog to choose new file
+        rename_dlg = wx.Dialog(parent=self, id=wx.ID_ANY, title=t("Select new file"))
+        rename_sizer = wx.BoxSizer(wx.VERTICAL)
+        path_dir = os.path.dirname(self.out_path)
+        path_file = os.path.basename(self.out_path)
+        self.file_crl = wx.FileCtrl(parent=rename_dlg, id=wx.ID_ANY, defaultDirectory=path_dir,
+                                    defaultFilename=path_file)
+        rename_sizer.Add(self.file_crl)
+        ok_btn = wx.Button(parent=rename_dlg, id=wx.ID_ANY, label=t("Ok"))
+        rename_sizer.Add(ok_btn, 0, wx.ALL | wx.ALIGN_CENTER_HORIZONTAL, 10)
+        rename_dlg.SetSizer(rename_sizer)
+        rename_dlg.SetAutoLayout(1)
+        rename_dlg.Fit()
+        rename_dlg.Bind(event=wx.EVT_BUTTON, handler=self.get_renamed_path, source=ok_btn)
+        rename_dlg.ShowModal()
+
+    def get_renamed_path(self, e):
+        # get the path
+        self.final_path = self.file_crl.GetPath()
+        # and destroy the new file name dialog
+        e.EventObject.Parent.Destroy()
 
     def open_file_with_app(self, e):
         # destroy the calling dialog
