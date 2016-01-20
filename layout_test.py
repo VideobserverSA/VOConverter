e869aa9ac30b0ff32a87702483b22a4ed5519fa6
Rui Pires <r.pires@digisource.pt>
2016-01-20 19:53:17 +0000

    reorganize layout code

--- layout_test.py
+++ layout_test.py
@@ -1,4 +1,5 @@
 import wx
+import webbrowser
 
 # flags_center = wx.SizerFlags(1)
 # flags_center.Center()
@@ -9,12 +10,14 @@ color_orange = wx.Colour(255, 122, 0)
 color_background_grey = wx.Colour(226, 228, 234)
 color_home_back = wx.Colour(243, 245, 250)
 color_home_text = wx.Colour(44, 49, 56)
+color_home_headers = wx.Colour(155, 160, 167)
 color_white = wx.WHITE
 color_black = wx.BLACK
 
 
 class MainWindow(wx.Frame):
 
+    # main init
     def __init__(self, parent, title):
 
         # we don't want to allow resizing
@@ -36,6 +39,28 @@ class MainWindow(wx.Frame):
 
         self.Show()
 
+    # navigation
+    def show_main(self, e):
+        print("MAIN!")
+        self.replace_view(self.create_main_screen())
+
+    def show_convert_join(self, e):
+        print("CONVERT / JOIN!")
+        self.replace_view(self.create_convert_join_screen())
+
+    def show_upload(self, e):
+        pass
+
+    def show_playlist(self, e):
+        pass
+
+    def show_convert(self, e):
+        pass
+
+    def show_join(self, e):
+        pass
+
+    # show the next screen
     def replace_view(self, new_view_creator):
         # remove the previous view
         self.current_window.Destroy()
@@ -53,6 +78,7 @@ class MainWindow(wx.Frame):
         self.SendSizeEvent()
         self.Update()
 
+    # create a nav button
     def create_button(self, parent, size, file, text, back_color, text_color, hover_color, click_handler):
 
         # a window so we can have a colored background
@@ -94,6 +120,7 @@ class MainWindow(wx.Frame):
         # component ready to use elsewhere
         return anchor_window
 
+    # to change the back of a nav button
     def change_background(self, window, color):
         window.SetBackgroundColour(color)
         self.Layout()
@@ -101,130 +128,7 @@ class MainWindow(wx.Frame):
         self.SendSizeEvent()
         self.Update()
 
-    def convert(self, e):
-        print("CONVERT!")
-        self.replace_view(self.create_second_screen())
-
-    def join(self, e):
-        print("JOIN!")
-        self.replace_view(self.create_second_screen())
-
-    def upload(self, e):
-        print("UPLOAD!!")
-        self.replace_view(self.create_main_screen())
-
-    def playlist(self, e):
-        print("PLAYLIST!!")
-        self.replace_view(self.create_main_screen())
-
-    def create_main_screen(self):
-
-        win = wx.Window(parent=self, id=wx.ID_ANY)
-        win.SetBackgroundColour(color_white)
-        # main sizer
-        sizer = wx.BoxSizer(orient=wx.VERTICAL)
-        win.SetSizer(sizer)
-
-        # place header
-        header_window = self.create_header(parent=win)
-        sizer.Add(header_window)
-
-        # what do you want to do?
-        select_window = wx.Window(parent=win, id=wx.ID_ANY, size=(600, 100))
-        select_window.SetBackgroundColour(color_white)
-        select_window_sizer = wx.BoxSizer(orient=wx.VERTICAL)
-        select_window.SetSizer(select_window_sizer)
-
-        select_text = wx.StaticText(parent=select_window, id=wx.ID_ANY, label="What do you want to do?")
-        select_text.SetFont(wx.Font(12, wx.FONTFAMILY_SWISS, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
-        select_text.SetForegroundColour(color_home_text)
-        select_window_sizer.Add(select_text, 0, wx.CENTER | wx.TOP, 40)
-        sizer.Add(select_window)
-
-        # create the buttons
-        button_sizer = wx.BoxSizer(orient=wx.HORIZONTAL)
-        button_sizer.SetMinSize((600, 200))
-
-        upload_btn = self.create_button(parent=win,
-                                        size=(190, 190),
-                                        file="assets/home_convert_file.png",
-                                        text="CONVERT FILE",
-                                        back_color=color_home_back,
-                                        text_color=color_home_text,
-                                        hover_color=color_orange,
-                                        click_handler=self.convert)
-
-        convert_btn = self.create_button(parent=win,
-                                         size=(190, 190),
-                                         file="assets/home_upload_file.png",
-                                         text="UPLOAD",
-                                         back_color=color_home_back,
-                                         text_color=color_home_text,
-                                         hover_color=color_orange,
-                                         click_handler=self.upload)
-
-        join_btn = self.create_button(parent=win,
-                                      size=(190, 190),
-                                      file="assets/home_create_playlist.png",
-                                      text="CREATE PLAYLIST",
-                                      back_color=color_home_back,
-                                      text_color=color_home_text,
-                                      hover_color=color_orange,
-                                      click_handler=self.playlist)
-
-        button_sizer.Add(upload_btn, 0, wx.LEFT | wx.TOP | wx.BOTTOM, 10)
-        button_sizer.Add(convert_btn, 0, wx.LEFT | wx.TOP | wx.BOTTOM, 10)
-        button_sizer.Add(join_btn, 0, wx.LEFT | wx.TOP | wx.BOTTOM, 10)
-        sizer.Add(button_sizer)
-
-        # select a funtion?
-        select_window = wx.Window(parent=win, id=wx.ID_ANY, size=(600, 100))
-        select_window.SetBackgroundColour(color_white)
-        select_window_sizer = wx.BoxSizer(orient=wx.VERTICAL)
-        select_window.SetSizer(select_window_sizer)
-
-        select_text = wx.StaticText(parent=select_window, id=wx.ID_ANY, label="Select a function.")
-        select_text.SetFont(wx.Font(12, wx.FONTFAMILY_SWISS, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
-        select_text.SetForegroundColour(color_home_text)
-        select_window_sizer.Add(select_text, 0, wx.CENTER | wx.TOP, 40)
-        sizer.Add(select_window)
-
-        footer_window = self.create_footer(parent=win)
-        sizer.Add(footer_window)
-
-        return win
-
-    def create_second_screen(self):
-
-        win = wx.Window(parent=self, id=wx.ID_ANY)
-
-        sizer = wx.BoxSizer(orient=wx.VERTICAL)
-
-        header_window = self.create_header(parent=win)
-        sizer.Add(header_window)
-
-        button_sizer = wx.BoxSizer(orient=wx.HORIZONTAL)
-        button_sizer.SetMinSize((600, 200))
-
-        convert_btn = self.create_button(parent=win,
-                                         size=(300, 190),
-                                         file="test_icon2.png",
-                                         text="Upload",
-                                         back_color=wx.BLUE,
-                                         text_color=wx.WHITE,
-                                         hover_color=color_orange,
-                                         click_handler=self.upload)
-
-        button_sizer.Add(convert_btn, 0, wx.LEFT | wx.TOP | wx.BOTTOM, 10)
-
-        sizer.Add(button_sizer)
-
-        footer_window = self.create_footer(parent=win)
-        sizer.Add(footer_window)
-
-        win.SetSizer(sizer)
-        return win
-
+    # include the header always
     def create_header(self, parent):
 
         anchor_window = wx.Window(parent=parent, id=wx.ID_ANY, size=(600, 60))
@@ -280,6 +184,7 @@ class MainWindow(wx.Frame):
 
         return anchor_window
 
+    # and include the footer always
     def create_footer(self, parent):
 
         # footer bar
@@ -303,10 +208,158 @@ class MainWindow(wx.Frame):
         help_text.SetForegroundColour(color_white)
         help_text.SetCursor(wx.Cursor(wx.CURSOR_HAND))
         anchor_window_sizer.Add(help_text, 0, wx.CENTER | wx.RIGHT, 20)
-
-
+        help_text.Bind(wx.EVT_LEFT_DOWN, self.go_to_help)
 
         return anchor_window
+
+    def go_to_help(self, e):
+        webbrowser.open("http://faqs.videobserver.com/", new=0, autoraise=True)
+
+    # create the home screen
+    def create_main_screen(self):
+
+        win = wx.Window(parent=self, id=wx.ID_ANY)
+        win.SetBackgroundColour(color_white)
+        # main sizer
+        sizer = wx.BoxSizer(orient=wx.VERTICAL)
+        win.SetSizer(sizer)
+
+        # place header
+        header_window = self.create_header(parent=win)
+        sizer.Add(header_window)
+
+        # what do you want to do?
+        select_window = wx.Window(parent=win, id=wx.ID_ANY, size=(600, 100))
+        select_window.SetBackgroundColour(color_white)
+        select_window_sizer = wx.BoxSizer(orient=wx.VERTICAL)
+        select_window.SetSizer(select_window_sizer)
+
+        select_text = wx.StaticText(parent=select_window, id=wx.ID_ANY, label="What do you want to do?")
+        select_text.SetFont(wx.Font(12, wx.FONTFAMILY_SWISS, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
+        select_text.SetForegroundColour(color_home_headers)
+        select_window_sizer.Add(select_text, 0, wx.CENTER | wx.TOP, 40)
+        sizer.Add(select_window)
+
+        # create the buttons
+        button_sizer = wx.BoxSizer(orient=wx.HORIZONTAL)
+        button_sizer.SetMinSize((600, 200))
+
+        upload_btn = self.create_button(parent=win,
+                                        size=(190, 190),
+                                        file="assets/home_convert_file.png",
+                                        text="CONVERT FILE",
+                                        back_color=color_home_back,
+                                        text_color=color_home_text,
+                                        hover_color=color_home_headers,
+                                        click_handler=self.show_convert_join)
+
+        convert_btn = self.create_button(parent=win,
+                                         size=(190, 190),
+                                         file="assets/home_upload_file.png",
+                                         text="UPLOAD",
+                                         back_color=color_home_back,
+                                         text_color=color_home_text,
+                                         hover_color=color_home_headers,
+                                         click_handler=self.show_upload)
+
+        join_btn = self.create_button(parent=win,
+                                      size=(190, 190),
+                                      file="assets/home_create_playlist.png",
+                                      text="CREATE PLAYLIST",
+                                      back_color=color_home_back,
+                                      text_color=color_home_text,
+                                      hover_color=color_home_headers,
+                                      click_handler=self.show_playlist)
+
+        button_sizer.Add(upload_btn)
+        button_sizer.Add(convert_btn, 0, wx.LEFT, 5)
+        button_sizer.Add(join_btn, 0, wx.LEFT, 5)
+        sizer.Add(button_sizer, 0, wx.LEFT | wx.RIGHT, 10)
+
+        # select a funtion?
+        select_window = wx.Window(parent=win, id=wx.ID_ANY, size=(600, 100))
+        select_window.SetBackgroundColour(color_white)
+        select_window_sizer = wx.BoxSizer(orient=wx.VERTICAL)
+        select_window.SetSizer(select_window_sizer)
+
+        select_text = wx.StaticText(parent=select_window, id=wx.ID_ANY, label="Select a function.")
+        select_text.SetFont(wx.Font(12, wx.FONTFAMILY_SWISS, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
+        select_text.SetForegroundColour(color_home_headers)
+        select_window_sizer.Add(select_text, 0, wx.CENTER | wx.TOP, 40)
+        sizer.Add(select_window)
+
+        footer_window = self.create_footer(parent=win)
+        sizer.Add(footer_window)
+
+        return win
+
+    # this is where you select convert a single file or join some
+    def create_convert_join_screen(self):
+
+        win = wx.Window(parent=self, id=wx.ID_ANY)
+        win.SetBackgroundColour(color_white)
+        # main sizer
+        sizer = wx.BoxSizer(orient=wx.VERTICAL)
+        win.SetSizer(sizer)
+
+        # place header
+        header_window = self.create_header(parent=win)
+        sizer.Add(header_window)
+
+        # what do you want to do?
+        select_window = wx.Window(parent=win, id=wx.ID_ANY, size=(600, 100))
+        select_window.SetBackgroundColour(color_white)
+        select_window_sizer = wx.BoxSizer(orient=wx.VERTICAL)
+        select_window.SetSizer(select_window_sizer)
+
+        select_text = wx.StaticText(parent=select_window, id=wx.ID_ANY, label="What do you want to do?")
+        select_text.SetFont(wx.Font(12, wx.FONTFAMILY_SWISS, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
+        select_text.SetForegroundColour(color_home_headers)
+        select_window_sizer.Add(select_text, 0, wx.CENTER | wx.TOP, 40)
+        sizer.Add(select_window)
+
+        # create the buttons
+        button_sizer = wx.BoxSizer(orient=wx.HORIZONTAL)
+        button_sizer.SetMinSize((600, 200))
+
+        join_btn = self.create_button(parent=win,
+                                      size=(280, 190),
+                                      file="assets/home_create_playlist.png",
+                                      text="JOIN FILES",
+                                      back_color=color_home_back,
+                                      text_color=color_home_text,
+                                      hover_color=color_home_headers,
+                                      click_handler=self.show_join)
+
+        convert_btn = self.create_button(parent=win,
+                                         size=(280, 190),
+                                         file="assets/home_convert_file.png",
+                                         text="CONVERT FILE",
+                                         back_color=color_home_back,
+                                         text_color=color_home_text,
+                                         hover_color=color_home_headers,
+                                         click_handler=self.show_convert)
+
+        button_sizer.Add(join_btn)
+        button_sizer.Add(convert_btn, 0, wx.LEFT, 5)
+        sizer.Add(button_sizer, 0, wx.LEFT | wx.RIGHT, 10)
+
+        # select a funtion?
+        select_window = wx.Window(parent=win, id=wx.ID_ANY, size=(600, 100))
+        select_window.SetBackgroundColour(color_white)
+        select_window_sizer = wx.BoxSizer(orient=wx.VERTICAL)
+        select_window.SetSizer(select_window_sizer)
+
+        select_text = wx.StaticText(parent=select_window, id=wx.ID_ANY, label="Select a function.")
+        select_text.SetFont(wx.Font(12, wx.FONTFAMILY_SWISS, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD, False))
+        select_text.SetForegroundColour(color_home_headers)
+        select_window_sizer.Add(select_text, 0, wx.CENTER | wx.TOP, 40)
+        sizer.Add(select_window)
+
+        footer_window = self.create_footer(parent=win)
+        sizer.Add(footer_window)
+
+        return win
 
 
 app = wx.App(False)
